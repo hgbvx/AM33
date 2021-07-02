933c70bf352600797f853f9e1520fe1c0845d04f
hgbvx <50869824+hgbvx@users.noreply.github.com>
2021-07-02 15:33:28 +0200

    Delete both_fast.py

--- both_fast.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-import json
-from bh1750 import readLight
-from bmp280 import readBME280All
-
-
-temperature,pressure,humidity = readBME280All()
-lux = readLight()
-print(pressure)
