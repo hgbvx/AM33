ca91ee06c07e6b9803da0a76d00b178db9e0cb7f
Michal <hgbvx33@gmail.com>
2021-07-03 14:21:02 +0200

    todo sleep

--- BH1750.c
+++ BH1750.c
@@ -1,9 +1,6 @@
 #include "BH1750.h"
 
 short read_lux(int fd) {
-	wiringPiI2CWrite(fd,0x10);
-	//sleep(1);
-
 	short data = wiringPiI2CReadReg16(fd,0x1b00);
 	unsigned char *bytes = (unsigned char *)&data;
 	data = (bytes[0] << 8) | bytes[1];
